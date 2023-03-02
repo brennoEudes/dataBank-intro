59b795845c2dbf7cb15cc8aba6f538d0f0d462fe
“Brenno <brennoeudes@gmail.com>
2023-03-01 23:45:38 -0300

    Criação CRUD

--- models/drink.model.js
+++ models/drink.model.js
@@ -1,11 +1,15 @@
-import {model, Schema} from "mongoose";
+import { model, Schema } from "mongoose";
 
-const drinkSchema = new Schema ({
-    drink_name: {type: String, names: ["Caipirinha", "Caipivodka"], required: true},
-    lime: {type: Number, min: 0.5, max: 1, required: true},
-    suggar_spoon: {type: Number, required: true},
-    ice_ml: {type: Number, min: 200, max: 400},
-    drink_type: {type: String, drinks: ["Cachaça","Vodka"], required: true}
+const drinkSchema = new Schema({
+  drink_name: {
+    type: String,
+    names: ["Caipirinha", "Caipivodka"],
+    required: true,
+  },
+  lime: { type: Number, min: 0.5, max: 1, required: true },
+  suggar_spoon: { type: Number, required: true },
+  ice_ml: { type: Number, min: 200, max: 400 },
+  drink_type: { type: String, drinks: ["Cachaça", "Vodka"], required: true },
 });
 
 const drinkModel = model("Drink", drinkSchema);
--- routes/drink.routes.js
+++ routes/drink.routes.js
@@ -4,17 +4,74 @@ import drinkModel from "../models/drink.model.js";
 const drinkRouter = express.Router();
 
 drinkRouter.post("/", async (req, res) => {
-    try {
+  try {
+    const newDrink = await drinkModel.create({ ...req.body });
 
-        const newDrink = await drinkModel.create({...req.body})
-
-        return res.status(201).json(newDrink);
-
-    } catch (err) {
-        console.log ("Erro no preparo do drink!");
-    }
-}); 
+    return res.status(201).json(newDrink);
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json("Erro no preparo do drink!");
+  }
+});
 
 //Continuar CRUD por aqui...
+
+drinkRouter.get("/all-drinks", async (req, res) => {
+  try {
+    const allDrinks = await drinkModel.find();
+
+    return res.status(201).json(allDrinks);
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json("Erro no preparo do drink!");
+  }
+});
+
+drinkRouter.get("/drinkId", async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const oneDrink = await drinkModel.findById(id);
+
+    if (!oneDrink) {
+      return res.status(404).json("Drink não encontrado!");
+    }
+
+    return res.status(201).json(oneDrink);
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json("Erro no preparo do drink!");
+  }
+});
+
+drinkRouter.delete("/delete/id", async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const deletedDrink = await drinkModel.findByIdAndDelete(id);
+
+    return res.status(201).json(deletedDrink);
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json("Erro no preparo do drink!");
+  }
+});
+
+drinkRouter.put("/edit/id", async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const updatedDrink = await drinkModel.findByIdAndUpdate(
+      id,
+      { ...req.body },
+      { new: true, runValidators: true }
+    );
+
+    return res.status(201).json(updatedDrink);
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json("Erro no preparo do drink!");
+  }
+});
 
 export default drinkRouter;
